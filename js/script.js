03b090632358575c9d3dc0cd8308475ac3ef9e21
anderleand <andersonleandro.desouza@gmail.com>
2021-02-26 10:19:00 -0300

    definir fim do jogo

--- js/script.js
+++ js/script.js
@@ -54,6 +54,13 @@ function iniciarJogo() {
         snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") 
         snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
     
     criaBG();
     criaCobrinha();
